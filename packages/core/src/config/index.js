a395a8052d0c9a582e1736b2bee277d171ee71fd
Miracle He <hmiinyu@sina.com>
2021-12-27 23:26:07 +0800

    实现core的type

--- packages/core/src/config/index.js
+++ packages/core/src/config/index.js
@@ -63,3 +63,17 @@ export const DEFAULT_STORAGE_OPTIONS = {
   cryptoKey: 'mate-key',
   cryptoIv: 'mate-iv'
 }
+/**
+ * @constant 正则表达式常量
+ */
+export const DATA_REGEX_PATTERN = {
+  guid: '^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}$',
+  mobile: '^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\d{8}$',
+  tel: '^(\\d{3,4}-\\d{7,8}-\\d{1,5})|(^\\d{3,4}-\\d{7,8})$',
+  email: '^[0-9a-z][_.0-9a-z-]{0,31}@([0-9a-z][0-9a-z-]{0,30}[0-9a-z]\\.){1,4}[a-z]{2,4}$',
+  idcard: '^\\d{18,18}|\\d{15,15}|\\d{17,17}x|\\d{17,17}X$',
+  secure: '^(.{before})(?:\\w+)(.{after})$',
+  letterNumber: '^(?=.*[0-9])(?=.*[a-zA-Z]).{min,max}$',
+  letterNumberChar: '^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9]).{min,max}$',
+  letterNumberCharCase: '^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[^a-zA-Z0-9]).{min,max}$'
+}
