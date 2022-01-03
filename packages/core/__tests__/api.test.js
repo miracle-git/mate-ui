70753aaa84130ff2b1298a820d78d7580d89bf83
Miracle He <hmiinyu@sina.com>
2022-01-03 11:16:17 +0800

    实现core的api

--- packages/core/__tests__/api.test.js
+++ packages/core/__tests__/api.test.js
@@ -149,3 +149,22 @@ test('测试：API地址映射(加密)', () => {
   expect(api.product.list).toEqual('/api/product/list')
   expect(window.__mate_api__).toEqual('f7842980a2c5019d5ab9eddc5db18cfeb4afd2f41177a84e68b882961992fcfb3bf086a211efc6214fbe3ff016beaf0b3518f3891887da9f9ea7d2f7bdd29063a951fdf79705a86c8053d78df2284f15c0756ec18353abbcf35086d9122627f7aff7785e680ddd3ea9471c30e13d759cf2412e1ef09c2beed48c22c513c3178f2a1b9d8958122d22abb5ab4f02df0e21533578f32cd1f2718e6838aeac27cb593d056f62f819e5efdd6cf2b3abe7f74390376d17760915d4fb1c2d5660b23352')
 })
+
+test('测试：API地址映射(动态参数)', () => {
+  const config = {
+    dict: '/dict/list',
+    user: {
+      list: '/user/list',
+      detail: '/user/detail/[id]'
+    },
+    product: {
+      list: '/product/list',
+      detail: '/product/detail',
+      category: '/product/category/[name]/[mode]'
+    }
+  }
+  window.__mate_api__ = ''
+  const api = Api.map(config, '/api')
+  expect(Api.format(api.user.detail, { id: '001' })).toEqual('/api/user/detail/001')
+  expect(Api.format(api.product.category, { name: 'huawei', mode: 'p40' })).toEqual('/api/product/category/huawei/p40')
+})
--- packages/core/src/api/index.js
+++ packages/core/src/api/index.js
@@ -3,10 +3,18 @@
  * @description 获取应用Api配置映射
  */
 import Crypto from '../crypto'
+import Type from '../type'
 import { mocking, mapping } from '../.internal/api'
 import { SYMMETRIC_CRYPTO_TYPE } from '../config'
 
 export default class Api {
+  /**
+   * @method 根据当前的地址配置生成Api对象
+   * @param config 地址配置(可按照模块划分)
+   * @param prefix 前缀(可为字符串或对象，默认为'/api')
+   * @param mock mock接口相关配置(自动将前缀修改为'/api/mock')
+   * @param crypto 是否在客户端加密存储(自动将存储到window.__mate_api__变量中)
+   */
   static map(config, prefix = '', { mock, crypto = false } = {}) {
     if (crypto) {
       const cryptoType = SYMMETRIC_CRYPTO_TYPE.DES
@@ -18,5 +26,14 @@ export default class Api {
       window.__mate_api__ = mapping(config, prefix, mocking(mock))
       return window.__mate_api__
     }
+  }
+  /**
+   * @method 将当前的Api地址的动态参数进行转化为实际的请求url
+   * @param url Api地址(包含动态参数，一般以[xx]形式存在，可包含多个不同占位的参数)
+   * @param config 参数的配置(即需要替换成的真实对象，一般以{xx:'yy'}形式存在)
+   */
+  static format(url, config) {
+    if (Type.isEmptyString(String(url)) || !Type.isObject(config)) return url
+    return Object.keys(config).reduce((r, s) => r = r.replace(new RegExp(`\\[${s}\\]`), config[s]), url)
   }
 }
