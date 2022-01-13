c68065379fba59c957e425b87ec08992c65f42b4
Miracle He <hmiinyu@sina.com>
2022-01-13 11:58:02 +0800

    更新http内部方法

--- packages/core/src/.internal/http.js
+++ packages/core/src/.internal/http.js
@@ -1,3 +1,27 @@
-export const getUrl = () => {}
+export const getUrl = (url, base, config) => {
+  let baseUrl = ''
+  if (config.base && base[config.base]) {
+    baseUrl = base[config.base]
+  }
+  if (baseUrl.endsWith('/')) {
+    baseUrl = baseUrl.slice(-1)
+  }
+  if (url.startsWith('/')) {
+    url = url.substr(1)
+  }
+  return `${baseUrl}/${url}`
+}
+
 export const getInstance = (interceptor, config) => {}
-export const handleParams = () => {}
+
+export const handleParams = (params, config, common) => {
+  let _params = params
+  let _config = config
+  if (!config）{
+    const { params, ...rest } = _params || {}
+    _params = params
+    _config = rest
+  }
+  _config = { ...common, ...(_config || {}) }
+  return { _params, _config } 
+}
