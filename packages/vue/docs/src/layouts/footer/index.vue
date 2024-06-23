96625df7aad1156125040aa9addae7a04795f652
miracle <hmiinyu@sina.com>
2024-06-23 22:56:16 +0800

    feat: delete all demo files of vue

--- packages/vue/docs/src/layouts/footer/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    Footer
-  </div>
-</template>
-<script setup lang="ts">
-</script>
--- packages/vue/src/components/button/docs/zh-CN.md
+++
@@ -1,8 +0,0 @@
----
-category: Components
-type: 通用
-title: Button
-subtitle: 按钮
----
-
-按钮用于开始一个即时操作。
